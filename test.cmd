fa976220f0af4aff24baa968fc5f002dd8974bcf
iyanke <63282359+iyanke@users.noreply.github.com>
2020-09-18 11:36:48 +0300

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,5 +1,5 @@
 echo "##teamcity[testStarted name='TestPackage1.test1']"
-rem echo "##teamcity[testFailed name='TestPackage1.test1' message='failure message' details='message and stack trace']"
+REM echo "##teamcity[testFailed name='TestPackage1.test1' message='failure message' details='message and stack trace']"
 echo "##teamcity[testFinished name='TestPackage1.test1']"
 
 echo "##teamcity[testStarted name='TestPackage1.test2']"
