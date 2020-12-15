da710c9b48a7e6ee6ab912718934e347ed9adc86
iyanke <63282359+iyanke@users.noreply.github.com>
2020-12-15 16:32:31 +0300

    zzzzz

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 echo "##teamcity[testStarted name='TestPackage1.test1']"
 echo "##teamcity[testFinished name='TestPackage1.test1']"
-//hhhввввввввввввввbbbb
+//hhhввввввввввввввbbbbzzzz
 echo "##teamcity[testStarted name='TestPackage1.test2']"
 echo "##teamcity[testFailed name='TestPackage1.test2' message='failure message' details='message and stack trace']"
 echo "##teamcity[testFinished name='TestPackage1.test2']"
