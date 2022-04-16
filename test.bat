d94467012a9676ac611674e1ac4bed1999b71266
Inna Yanke <63282359+iyanke@users.noreply.github.com>
2022-04-17 01:03:07 +0300

    abcdefg

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 for /l %%x in (6, 1, 25) do  (
 echo "test%%x" >> "C:\!\git\tests\br3"
 git add .  
-git commit -m "tests changed %%xtestssssssssssssssss"
+git commit -m "tests changed %%xtestsssssssssssxssssssssss"
 git push https://iyanke:PutiN20!9@github.com/iyanke/tests --all 
 )
