7338650dad2e21b8dafbd72b6724ec49a486ef5b
Inna Yanke <63282359+iyanke@users.noreply.github.com>
2022-05-04 15:10:16 +0300

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,7 +1,7 @@
 echo "##teamcity[testStarted name='TestPackage1.test1']"
 
 echo "##teamcity[testFinished name='TestPackage1.test1']"
-//hhhввввввввввввввbbbbzzzzddffff
+//hhh
 echo "##teamcity[testStarted name='TestPackage1.test2']"
 echo "##teamcity[testFailed name='TestPackage1.test2' message='failure message' details='message and stack trace']"
 echo "##teamcity[testFinished name='TestPackage1.test2']"
